c955b1fd615a98f6efae5601d0415bd96bb283e0
samuel <samuel.bagattin@gmail.com>
2020-05-06 12:11:42 +0200

    😀

--- TP1/Main.java
+++ TP1/Main.java
@@ -1,5 +1,9 @@
 public class Main{
+
+    /**
+     * @param args Le nom de la personne
+     */
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(String.format("Bonjour %s", args[0]));
     }
 }
---
+++ TP1/readme.md
@@ -0,0 +1,7 @@
+## TP1
+```bash
+javadoc Main.java
+javac Main.java
+jar cfm Main.jar .\META-INF\MANIFEST.MF Main.class
+java -jar Main.jar Samuel
+```
